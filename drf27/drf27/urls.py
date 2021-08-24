f8b0f9cf039ed3a47d246e28bed0de43a945fe29
Manvi <msinghal3102@gmail.com>
2021-08-25 00:09:10 +0530

    tokenauthentication

--- drf27/drf27/urls.py
+++ drf27/drf27/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
 #from rest_framework.authtoken.views import obtain_auth_token
-from api.auth import CustomAuthToken
+#from api.auth import CustomAuthToken
 
 router=DefaultRouter()
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('gettoken/', CustomAuthToken.as_view()),
+    #path('gettoken/', CustomAuthToken.as_view()),
 ]
+
